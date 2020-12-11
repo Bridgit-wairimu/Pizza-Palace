5dd044a1837a0b5c332dbb9559f4bf1942fb45e2
Bridgit-wairimu <kirikabetty@gmail.com>
2020-12-11 05:32:48 +0300

    deleted the pizza.hmtl route and the about.html route

--- app/main/views.py
+++ app/main/views.py
@@ -11,19 +11,4 @@ def index():
     title = "Welcome to Pizza Palace"
     return render_template('index.html', title = title)
     
-@main.route('/pizza')
-def pizza():
 
-    """
-    view root page function that returns the index page and its data
-    """
-    return render_template('pizza.html')
-        
-
-@main.route('/about')
-def about():
-
-    """
-    view root page function that returns the index page and its data
-    """
-    return render_template('about.html')
