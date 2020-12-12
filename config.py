3d183494b2bdd84d26184616bfc94e4b1c82f1fc
Alikhalid2020 <lanrakhaled@gmail.com>
2020-12-12 11:36:14 +0300

    merge changes

--- config.py
+++ config.py
@@ -1,27 +1,29 @@
-class Config:
-    '''
-    General configuration parent class
-    '''
-    pass
+import os
 
+
+class Config:
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://morninga:Alikhalid3436@localhost/pizza'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
-
-
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
+
+ # email configurations
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS=True
+    MAIL_USERNAME=os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME="lanrakhaled@gmail.com"
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
